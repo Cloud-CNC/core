34661f6ccdbb0afb54d26d302cda9804e957d336
Wakeful Cloud <wakefulcloud@gmail.com>
2020-01-29 19:42:26 -0700

    Fixed tests

--- app/tests/index.js
+++ app/tests/index.js
@@ -2,17 +2,21 @@
  * @fileoverview Tests
  */
 
+//Environment variable (NOTE: Must be loaded before config)
+process.env.NODE_ENV = 'testing';
+
 //Imports
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const syswideCA = require('syswide-cas');
+const {core} = require('../../config.js');
 
-//Environment variable
-process.env.NODE_ENV = 'testing';
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+//Enable self signed certificate
+syswideCA.addCAs(core.cert);
 
 //Chai
 chai.use(chaiHttp);
 
 //Tests
 describe('Unit', require('./unit/index.js'));
-//describe('Integration', require('./integration/index.js'));
+describe('Integration', require('./integration/index.js'));
