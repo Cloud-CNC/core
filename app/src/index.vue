bf08dbfb411a2e7d63bc9793bb8debdc21172947
Wakeful Cloud <wakefulcloud@gmail.com>
2020-01-30 17:40:55 -0700

    Workflow Cleanup Fix 2

--- app/src/index.vue
+++ app/src/index.vue
@@ -59,7 +59,7 @@
 
       <v-list-item>
         <v-list-item-icon>
-          <v-icon>remove_outline</v-icon>
+          <v-icon>delete</v-icon>
         </v-list-item-icon>
         <v-list-item-content>
           <v-btn
